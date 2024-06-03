36694688e359874176deac8266bcc4ba5ef0d0b8
Kevin Grastorf <kevin.grastorf@gmail.com>
2024-06-03 12:48:17 -0600

    Expense Tracker first commit

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../models/expense.dart' as prefix;
+import '../models/expense.dart';
+import 'expenses_list/expenses_list.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: prefix.Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: prefix.Category.leisure),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        //
+
+        children: [
+          const Text('The Chart'),
+          Expanded(
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
